178612959a1b0c14d6a4b8bc7efd39408544f555
Adam <ayhaadam@gmail.com>
2017-11-17 09:13:47 +0800

    Initial commit

---
+++ src/plugin/my-component.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="my-style">{{greetings}} World !</p>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      greetings: 'Hello'
+    }
+  }
+}
+</script>
+
+<style lang=css>
+.my-style {
+ font-size: 2em;
+ text-align: center;
+}
+</style>
